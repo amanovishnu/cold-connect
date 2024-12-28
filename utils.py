2a2f1b4c3c4c3966aeb663e84a55d67414139d07
amanovishnu <geekymano@gmail.com>
2024-12-28 21:31:11 +0530

    info: add text cleaning utility function

--- utils.py
+++ utils.py
@@ -1,4 +1,15 @@
+import re
 import requests
+
+
+def clean_text(text):
+    text = re.sub(r'<[^>]*?>', '', text)
+    text = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', text)
+    text = re.sub(r'[^a-zA-Z0-9 ]', '', text)
+    text = re.sub(r'\s{2,}', ' ', text)
+    text = text.strip()
+    text = ' '.join(text.split())
+    return text
 
 
 def get_model_list():
@@ -9,9 +20,7 @@ def get_model_list():
         "Content-Type": "application/json"
     }
     response = requests.get(url, headers=headers)
-
     model_list: list = []
-
     for model in response.json()['data']:
         model_list.append(model['id'])
     return model_list
